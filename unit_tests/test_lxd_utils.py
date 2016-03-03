c0efd96701c77c7e62db1e372fe9c3c5d4fbcffc
Paul Hummer <paul@eventuallyanyway.com>
2016-03-03 08:21:08 -0700

    Fix lxd thinpool creation.
    Force creation of the lxd thinpool.
    Re-enable lvm tests.
    Ignore charm temp and pyc files.
    
    Change-Id: I8887f3231e789ab58c548ba583740035151097aa
    
    Fix lxd thinpool creation.
    
    Force creation of the lxd thinpool.
    Re-enable lvm tests.
    Ignore charm temp and pyc files.
    
    Change-Id: I8887f3231e789ab58c548ba583740035151097aa
    
    Add simplejson to requirements.
    
    Change-Id: I2bc3992a020892fbd5af139f538d6e2069496ef2
    
    Add unit tests
    
    Change-Id: Iaeaa554eb05e2dd015ea699741a3d4d378e7dbc1
    
    Fix lint.
    
    Change-Id: I5e8a2b94ed434cf5338eb3925cd3a7342cc85360
    
    Unify both Makefile and tox calls.
    
    Change-Id: I0c8b35e5d777ea221a34521a258e26c4029fdccd

---
+++ unit_tests/test_lxd_utils.py
@@ -0,0 +1,81 @@
+"""Tests for hooks.lxd_utils."""
+import os
+
+import mock
+
+import lxd_utils
+import testing
+
+
+class TestLXDUtilsDeterminePackages(testing.CharmTestCase):
+    """Tests for hooks.lxd_utils.determine_packages."""
+
+    TO_PATCH = [
+        'config',
+    ]
+
+    def setUp(self):
+        super(TestLXDUtilsDeterminePackages, self).setUp(
+            lxd_utils, self.TO_PATCH)
+        self.config.side_effect = self.test_config.get
+
+    def test_determine_packages(self):
+        """A list of LXD packages should be returned."""
+        expected = [
+            'btrfs-tools',
+            'thin-provisioning-tools',
+            'criu',
+            'lvm2',
+            'lxd',
+            'lxd-client',
+        ]
+
+        packages = lxd_utils.determine_packages()
+
+        self.assertEqual(expected, packages)
+
+
+class TestLXDUtilsCreateAndImportBusyboxImage(testing.CharmTestCase):
+    """Tests for hooks.lxd_utils.create_and_import_busybox_image."""
+
+    TO_PATCH = []
+
+    def setUp(self):
+        super(TestLXDUtilsCreateAndImportBusyboxImage, self).setUp(
+            lxd_utils, self.TO_PATCH)
+
+    @mock.patch('lxd_utils.subprocess.Popen')
+    @mock.patch('lxd_utils.shutil.rmtree')
+    @mock.patch('lxd_utils.subprocess.call')
+    @mock.patch('lxd_utils.tarfile.open')
+    @mock.patch('lxd_utils.tempfile.mkdtemp')
+    @mock.patch('lxd_utils.check_call')
+    def test_create_and_import_busybox_image(
+            self, check_call, mkdtemp, tarfile_open, subprocess_call,
+            rmtree, Popen):
+        """A busybox image is imported into lxd."""
+        mkdtemp.return_value = '/not/a/real/path'
+        tarfile_open.return_value = mock.Mock()
+        subprocess_call.return_value = False
+        Popen_rv = mock.Mock()
+        Popen_rv.stdout.read.return_value = '\n'
+        Popen.return_value = Popen_rv
+
+        lxd_utils.create_and_import_busybox_image()
+
+        self.assertTrue(check_call.called)
+        args = check_call.call_args[0][0]
+        self.assertEqual(['lxc', 'image', 'import'], args[:3])
+        self.assertEqual(['--alias', 'busybox'], args[4:])
+        self.assertFalse(os.path.exists(args[3]))
+
+        # Assert all other mocks *would* have been called.
+        mkdtemp.assert_called_once_with()
+        tarfile_open.assert_called_once_with(
+            '/not/a/real/path/busybox.tar', 'w:')
+        subprocess_call.assert_called_once_with(
+            ['xz', '-9', '/not/a/real/path/busybox.tar'])
+        Popen.assert_called_once_with(
+            ['/bin/busybox', '--list-full'], stdout=-1,
+            universal_newlines=True)
+        Popen_rv.stdout.read.assert_called_once_with()
